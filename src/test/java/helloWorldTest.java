f53dfe5f95747e7fd6b7cf32c7a8e204ca3caf11
MichailVid <Mvidenyapin@mail.ru>
2025-04-24 08:40:01 +0300

    Тест

--- src/test/java/helloWorldTest.java
+++ src/test/java/helloWorldTest.java
@@ -1,9 +1,13 @@
+import io.restassured.RestAssured;//
+import io.restassured.response.Response;//
 import org.junit.jupiter.api.Test;
 
 public class helloWorldTest {
     @Test
     public void testHelloWorld(){
-    System.out.println("Hello World");
+    Response response = RestAssured
+            .get("https://playground.learnqa.ru/api/get_text")//Гет запрос на адрес
+            .andReturn();//вернуть результат в переменную response
+    response.prettyPrint();//вывести полученый json
 }
-sssss
 }
